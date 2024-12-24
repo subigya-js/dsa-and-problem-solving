0a6e3879941a637911587fa3cf7c78bc7310867a
SUBIGYA SUBEDI <developer.subigya@gmail.com>
2024-12-24 23:48:09 +0530

    Hashing added

---
+++ Hashing/basis.java
@@ -0,0 +1,11 @@
+package Hashing;
+
+import java.util.Scanner;
+
+public class basis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+    }
+}
