bed89e9d939315f92c266f2ae24083b8c2e1b10d
subigya-js <developer.subigya@gmail.com>
2025-04-14 11:23:59 +0530

    Refactor Basis class and improve number repetition check

--- Hashing/basis.java
+++ Hashing/basis.java
@@ -1,11 +1,23 @@
-package Hashing;
-
 import java.util.Scanner;
 
-public class basis {
+public class Basis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int[] num = { 1, 2, 3, 5, 2, 1, 8 };
+
+        // Check how many times a specific number is repeated.
+        System.out.printf("Enter the number: ");
+        int val = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            if(num[i] == val) {
+                count ++;
+            }
+        }
+
+        System.out.println(val + " is repeated for: " + count + " times");
     }
 }
