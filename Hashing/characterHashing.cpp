406578a38544983fe08ec7180bbdf1ce071d6b9d
subigya-js <developer.subigya@gmail.com>
2025-04-20 09:03:08 +0530

    Add character hashing implementation

--- Hashing/characterHashing.cpp
+++ Hashing/characterHashing.cpp
@@ -3,9 +3,32 @@ using namespace std;
 
 int main()
 {
-    int a = 'a';
+    string s;
+    cout << "Enter the string:";
+    cin >> s;
 
-    cout << "The value of a is: " << a << endl;
+    // pre compute
+    int hash[26] = {0};
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i] - 'a']++;
+    }
+
+    int q;
+    cout << "Enter the number queries: ";
+    cin >> q;
+
+    cout << "Enter query elements:" << endl;
+
+    while (q--)
+    {
+        char c;
+        cin >> c;
+
+        // fetch
+        cout << c << " occurs for " << hash[c - 'a'] << " times" << endl;
+    }
 
     return 0;
 }
